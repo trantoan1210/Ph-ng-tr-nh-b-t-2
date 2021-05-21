494018eb36cd2fac243f693dd0a89ce5fde3ed8e
Toàn Trần <neo290992@gmail.com>
2021-05-21 16:14:29 +0700

    nhập bang

---
+++ my.js
@@ -0,0 +1,32 @@
+function number() {
+let number1 = document.getElementById('number1').value;
+let number2 = document.getElementById('number2').value;
+let number3 = document.getElementById('number3').value;
+number4 = [(number2**2) - (4 * number1 * number3)];
+
+if (number1==0){
+    if (number2==0) {
+ document.getElementById('ketqua').innerHTML = 'Mày ngu quá tự trả lời đi';
+    }
+    else {
+       boss= ([-number3] / number2)
+ document.getElementById('ketqua').innerHTML = 'X=' + boss  ; 
+}}
+else {
+    if (number4<=0){
+        document.getElementById('ketqua').innerHTML = 'Vô nghiệm';
+    }
+    else{
+        if (number4==0){
+        nood = (-number2/(2*number1));
+        document.getElementById('ketqua').innerHTML = 'X='+ nood;
+        }
+        else{
+            X1= ((-number2+Math.sqrt(number4))/(2 *number1));
+            X2= ((-number2-Math.sqrt(number4))/(2 *number1));
+
+            document.getElementById('ketqua').innerHTML = 'X1='+X1+',X2='+X2;
+        }
+    }
+}
+}
